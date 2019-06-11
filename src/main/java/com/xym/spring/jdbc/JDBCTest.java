2b0ec52b24d8156d48014d301b062378a75a8579
xym760 <1455422076@qq.com>
2019-06-11 17:18:23 +0800

    JdbcTemplate练习

--- src/main/java/com/xym/spring/jdbc/JDBCTest.java
+++ src/main/java/com/xym/spring/jdbc/JDBCTest.java
@@ -70,7 +70,7 @@ public class JDBCTest {
     }
 
     /**
-     * 执行批量更新：insert，update，delete
+     * 执行批量更新：insert，update，delete等
      * 最后一个参数是Object[]类型的List，因为一条记录对应一个Object[]，多条记录就对应一个Object数组类型的List
      */
     @Test
